e77fc904cc016ae1a13272e44964a15aff08195a
ClouD <cvestini@gmail.com>
2024-10-13 17:01:29 +0100

    Sixth Version:
    - parameter beta can now be changed at each iteration step. if we are outside the intersection, beta is set to 0 and we use MAP

---
+++ Version 6/path_plotter.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+
+def plot_path(path: list, ax) -> None:
+    """Plots the path followed by Dykstra's algorithm during projection.
+
+    Args:
+        path: A list of points representing the intermediate steps
+              taken by the algorithm.
+        ax: Axes handle for plotting
+    """
+    # Extract x and y coordinates from the path
+    x_coords = [point[0] for point in path] # we love list comprehensions
+    y_coords = [point[1] for point in path]
+
+    # Plot the path
+    ax.plot(x_coords, y_coords, marker='.', linestyle='--',
+             color='blue', linewidth=0.5, markersize=1,
+             label='Projection Path')
+
+    # Add legend
+    ax.legend()
