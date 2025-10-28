7cc0a01ac306b1fd57749bc81ff5760136fd6ff0
claudio vestini <cvestini@gmail.com>
2025-10-28 13:17:46 -0400

    BUG FIXED for dykstra_MAP hybrid

--- Python/Working Version/dykstra_MAP_hybrid.py
+++ Python/Working Version/dykstra_MAP_hybrid.py
@@ -68,9 +68,14 @@ def dykstra_projection(z: np.ndarray, N: np.ndarray, c: np.ndarray,
 
     # Vector for storing all errors (V8)
     # if plot_errors:
-    errors_for_plotting = np.array([np.zeros_like(e_dykstra)
-                                    for _ in range(max_iter)])
+    # errors_for_plotting = np.array([np.zeros_like(e_dykstra)
+    #                                 for _ in range(max_iter)])
     # print(f"Errors for plotting {errors_for_plotting}") for debugging
+
+    # BUGFIX: Previous version
+    errors_for_plotting = [e_MAP.copy()] # initialise with all zeros
+    path = [z.copy()]  # Initialize the path with the original point
+    
 
     # Matrix of successive projections
     x_historical = np.array([[np.zeros_like(z) for _ in range(n)] 
@@ -120,12 +125,18 @@ def dykstra_projection(z: np.ndarray, N: np.ndarray, c: np.ndarray,
             # Update e_m with Dykstra's method
             e_dykstra[m] = e_dykstra[index]+ (x_temp - x)
 
-            # Path
-            x_historical[i][m] = x.copy()
+            # # Path
+            # x_historical[i][m] = x.copy()
+            # BUGFIX: Previous version
+            path.append(x.copy())  # Add the updated x to the path
+
 
             # Errors
             if plot_errors:
-                errors_for_plotting[i][m] = e[m].copy()
+                # errors_for_plotting[i][m] = e[m].copy()
+                # BUGFIX: Previous version
+                errors_for_plotting.append(e.copy()) # update error matrix
+
 
         # Track the squared error (V4)
         if track_error:
@@ -150,8 +161,8 @@ def dykstra_projection(z: np.ndarray, N: np.ndarray, c: np.ndarray,
             # Append error
             squared_errors[i] = error
 
-    # Path
-    path = x_historical.copy()
+    # # Path
+    # path = x_historical.copy()
 
     if track_error and plot_errors and plot_active_halfspaces:
         error_tuple = (squared_errors, stalled_errors, converged_errors)
